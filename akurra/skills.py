714aa3066ce1b44c993f500d53ef8f870a57cf27
revok <bytechomper@gmail.com>
2015-08-29 14:52:08 +0200

    spawn fireball skill on appropriate side of player collision core rect

--- akurra/skills.py
+++ akurra/skills.py
@@ -1,7 +1,7 @@
 """Skills module."""
 import logging
 
-from .entities import EntityEvent, EntityInput, EntityInputChangeEvent, System
+from .entities import EntityEvent, EntityInput, EntityCollisionEvent, EntityInputChangeEvent, System
 from .utils import unit_vector_between
 
 
@@ -30,8 +30,11 @@ class SkillUsageSystem(System):
     ]
 
     event_handlers = {
-        EntityInputChangeEvent: ['on_entity_event', 10]
+        EntityInputChangeEvent: ['on_entity_event', 10],
+        EntityCollisionEvent: ['on_entity_collision_event', 10]
     }
+
+    used_skill_ids = []
 
     def update(self, entity, event=None):
         """Have an entity updated by the system."""
@@ -47,12 +50,67 @@ class SkillUsageSystem(System):
         target = entity.components['input'].input[EntityInput.TARGET_POINT]
         direction = unit_vector_between(source, target)
 
-        fireball.components['position'].primary_position = source
-        fireball.components['velocity'].direction = direction
-        fireball.components['velocity'].speed = 150
+        # determine which side of the collision rect to spawn the fireball on
+        center = direction[0] == 0 and direction[1] == 0
+        # exempt center, since the player would shoot himself
+        if not center:
+            left = direction[0] < 0
+            bottom = direction[1] > 0
+            top = direction[1] < 0
 
-        layer.add_entity(fireball)
+            fireball_collision_pad = 5
+            fireball_width = fireball.components['physics'].collision_core.width + fireball_collision_pad
+            fireball_height = fireball.components['physics'].collision_core.height + fireball_collision_pad
 
-        # Trigger a skill usage event
-        # @TODO Add skill ID and state here
-        self.events.dispatch(EntitySkillUsageEvent(entity.id))
+            if left:
+                if bottom:
+                    # spawn fireball bottom left of player collision rect
+                    logging.info('bottom left')
+                    fireball_spawn_position = list(entity.components['physics'].collision_core.bottomleft)
+                    fireball_spawn_position[1] += fireball_height
+                elif top:
+                    logging.info('top left')
+                    # spawn fireball top left of player collision rect
+                    fireball_spawn_position = list(entity.components['physics'].collision_core.topleft)
+                    fireball_spawn_position[1] -= fireball_height
+                else:
+                    logging.info('left')
+                    # spawn fireball left of player collision rect
+                    fireball_spawn_position = list(entity.components['physics'].collision_core.left)
+                fireball_spawn_position[0] -= fireball_width
+            else:
+                if bottom:
+                    logging.info('bottom right')
+                    # spawn fireball bottom right of player collision rect
+                    fireball_spawn_position = list(entity.components['physics'].collision_core.bottomright)
+                    fireball_spawn_position[1] += fireball_height
+                elif top:
+                    logging.info('top right')
+                    # spawn fireball top right of player collision rect
+                    fireball_spawn_position = list(entity.components['physics'].collision_core.topright)
+                    fireball_spawn_position[1] -= fireball_height
+                else:
+                    logging.info('right')
+                    # spawn fireball righ of player collision rect
+                    fireball_spawn_position = list(entity.components['physics'].collision_core.right)
+                fireball_spawn_position[0] += fireball_width
+
+
+            fireball.components['position'].primary_position = fireball_spawn_position
+            fireball.components['velocity'].direction = direction
+            fireball.components['velocity'].speed = 150
+
+            layer.add_entity(fireball)
+
+            # Trigger a skill usage event
+            # @TODO Add skill ID and state here
+            self.events.dispatch(EntitySkillUsageEvent(entity.id))
+            self.used_skill_ids.append(entity.id)
+
+    def on_entity_collision_event(self, event):
+        """Handle an event which contains a reference to an entity."""
+        original_entity_id = event.entity_id
+        collided_entity_id = event.collided_entity_id
+
+        if original_entity_id in self.used_skill_ids:
+            logging.debug('FIREBALL HIT %s', original_entity_id)
